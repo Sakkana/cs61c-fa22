b1368c937363a9b93e8e8a197a3b81b04330e87c
Sakkana <642835027@qq.com>
2023-01-18 23:43:12 +0800

    finish lab1

--- lab01/exercise3/ll_cycle.c
+++ lab01/exercise3/ll_cycle.c
@@ -1,6 +1,44 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <assert.h>
 #include "ll_cycle.h"
 
+
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    int turn = 1;
+    node *fast = head;
+    node *slow = head;
+    while (fast != NULL && fast->next != NULLi) {
+        assert(fast);
+        if (turn == 1) {
+            if (fast->next) {
+                fast = fast->next;
+            }
+
+            assert(fast);
+
+            if (fast->next) {
+                fast = fast->next;
+            }
+
+            assert(fast);
+
+            //printf("fast in %d\n", fast->value);
+
+        } else {
+            slow = slow->next;
+
+            assert(slow);
+
+            //printf("slow in %d\n", slow->value);
+        }
+
+        turn = 1 - turn;
+
+        if (fast == slow) {
+            return 1;
+        }
+    }
+
+    return 0;
 }
